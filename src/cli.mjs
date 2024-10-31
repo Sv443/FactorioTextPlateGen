e0729fd50ae85f5290f666324de337aef3e2610f
Sv443 <contact@sv443.net>
2024-10-31 01:17:59 +0100

    ref: update cli script

--- src/cli.mjs
+++ src/cli.mjs
@@ -6,3 +6,10 @@ import { join } from "node:path";
 const child = spawn("node", [join(import.meta.dirname, "../dist/src/index.js")], {
   stdio: "inherit",
 });
+
+child.on("exit", (code, signal) => {
+  if(code)
+    setImmediate(() => process.exit(code));
+  else if(signal)
+    setImmediate(() => process.kill(process.pid, signal));
+});
